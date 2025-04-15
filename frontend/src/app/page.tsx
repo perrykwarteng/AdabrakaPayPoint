e810562e185e84453719995abd0d0eed5db5abf1
Perry John Kwarteng <perrykwarteng8@gmail.com>
2025-04-15 09:45:10 +0000

    fixed the padding

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,15 +3,13 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <div className="px-5">
-        <h1 className="my-5 text-[30px] text-center font-medium">Home</h1>
-        <Button type="submit" className="w-full md:w-[250px]">
-          <Link className="text-white" href="/payments">
-            Make Your payment here
-          </Link>
-        </Button>
-      </div>
+    <div className="min-h-screen flex flex-col items-center justify-center px-4">
+      <h1 className="my-5 text-[30px] text-center font-medium">Home</h1>
+      <Button type="submit" className="w-full md:w-[250px]">
+        <Link className="text-white" href="/payments">
+          Make Your payment here
+        </Link>
+      </Button>
     </div>
   );
 }
